9b689817b39500691be6dbbcf2023f59bdae88ed
Callum MacInnes <callum.macinnes89@gmail.com>
2025-06-01 22:44:13 +0100

    Update page.tsx

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -8,13 +8,13 @@ import ReplyForm from '../../components/ReplyForm'
 import { getUserFromServerCookie } from '../../../lib/server-auth'
 import MatchList from '../../components/MatchList'
 
-interface EventPageProps {
+type Props = {
   params: {
     slug: string;
   };
-}
+};
 
-export default async function EventPage({ params }: EventPageProps) {
+export default async function EventPage({ params }: Props) {
     const slug = params.slug // ✅ You do NOT await `params`
     const event = await prisma.event.findUnique({
         where: { slug },
